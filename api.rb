345dd4d10023b5257643315cea5264f9e461e8cc
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-07 09:15:31 -0800

    added gemfile

--- api.rb
+++ api.rb
@@ -5,28 +5,29 @@ require 'json'
 
 url = 'http://localhost:5984/mapuris'
 
-before do
-  puts '[Params]'
-  p params
-end
+# before do
+#   puts '[Params]'
+#   p params
+# end
 
 before do
   headers "Content-Type" => "application/json; charset=utf8"
 end
 
 get '/' do
-	{ "message" => 'Hello world!' }.to_json
+	redirect '/heartbeat'
 end
 
 get "/heartbeat" do
-	{ "status" => "ok" }.to_json
+	return JSON.pretty_generate({ "status" => "ok" })
 end
 
 get '/links/*' do
-	puts params[:splat]
 	doi = params[:splat].first
 	doi = doi.sub("/", "%2F")
 	url2 = url + '/' + doi
 	res = HTTParty.get(url2)
-	return JSON.pretty_generate(res.to_s)
+	body = JSON.parse(res.body)
+	out = { "status" => "ok", "data" => body }
+	return JSON.pretty_generate(out)
 end
